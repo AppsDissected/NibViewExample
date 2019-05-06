d039ebb29d1ae28f0cc21d9ffd303ba349ce66c0
Evan Deaubl <evan@deaubl.name>
2019-05-06 12:01:26 -0700

    Add code

---
+++ NibViewExample/MyCustomNIBView.swift
@@ -0,0 +1,11 @@
+//
+//  MyCustomNIBView.swift
+//  NibViewExample
+//
+//  Created by Evan Deaubl on 5/6/19.
+//  Copyright © 2019 Apps Dissected. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class MyCustomNIBView: NibView { }
